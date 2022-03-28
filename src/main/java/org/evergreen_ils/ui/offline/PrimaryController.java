27fc3c1925b90db00bec474775d495910a334f12
Bill Erickson <berickxx@gmail.com>
2022-03-28 09:24:29 -0400

    progressbar timer stuff
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- src/main/java/org/evergreen_ils/ui/offline/PrimaryController.java
+++ src/main/java/org/evergreen_ils/ui/offline/PrimaryController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
 
 import java.util.Locale;
@@ -16,10 +17,15 @@ public class PrimaryController {
     @FXML Tab tab1;
     @FXML Tab tab2;
     @FXML VBox bodyVbox;
+    @FXML ProgressBar progressBar;
 
     @FXML private void initialize() {
         setupStrings();
         setupData();
+    }
+
+    @FXML private void test() {
+        showProgressTimer(10); // TODO
     }
 
     void setupData() {
@@ -49,5 +55,43 @@ public class PrimaryController {
 
     @FXML private void close(ActionEvent event) {
         Platform.exit();
+    }
+
+    void showProgress() {
+        progressBar.setVisible(true);
+    }
+
+    void setProgress(float p) {
+        progressBar.setProgress(p);
+    }
+
+    void hideProgress() {
+        progressBar.setVisible(false);
+    }
+
+    // TODO return the thread so it can be stopped?
+    void showProgressTimer(float seconds) {
+
+        class Timer implements Runnable {
+            public void run() {
+
+                float t = 1;
+                while (t <= seconds) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (java.lang.InterruptedException e) {
+                        hideProgress();
+                        return;
+                    }
+                    float duration = t / seconds;
+                    setProgress(duration);
+                    t++;
+                }
+                hideProgress();
+            }
+        }
+
+        showProgress();
+        new Thread(new Timer()).start();
     }
 }
